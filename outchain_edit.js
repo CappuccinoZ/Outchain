587ffb7f23c4a848707611869990a968acba47e8
CappuccinoZ <43778933+CappuccinoZ@users.noreply.github.com>
2022-12-28 16:58:40 +0800

    Add files via upload

--- outchain_edit.js
+++ outchain_edit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         outchain edit
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  编辑音乐外链
 // @author       Aozaki
 // @match        https://www.keyfc.net/bbs/postreply.aspx?*
@@ -10,19 +10,19 @@
 // ==/UserScript==
 
 function checkURL(url) {
-    let url_list = ["//music.163.com/outchain", "music.163.com/outchain", "https://music.163.com/outchain",
-        "https://music.163.com/#/song?", "https://music.163.com/song?", "https://y.music.163.com/m/song?",
-        "https://music.163.com/#/playlist?", "https://music.163.com/#/my/m/music/playlist?", "https://y.music.163.com/m/playlist?",
-        "https://music.163.com/#/album?", "https://music.163.com/album?", "https://y.music.163.com/m/album?",
-        "https://music.163.com/#/djradio?", "https://music.163.com/djradio?", "https://y.music.163.com/m/djradio?"]
-    if (typeof (url) == "string") {
-        if (url.includes("music")) {
+    let url_list = ['//music.163.com/outchain', 'music.163.com/outchain', 'https://music.163.com/outchain',
+        'https://music.163.com/#/song?', 'https://music.163.com/song?', 'https://y.music.163.com/m/song?',
+        'https://music.163.com/#/playlist?', 'https://music.163.com/playlist?', 'https://y.music.163.com/m/playlist?', 'https://music.163.com/#/my/m/music/playlist?',
+        'https://music.163.com/#/album?', 'https://music.163.com/album?', 'https://y.music.163.com/m/album?',
+        'https://music.163.com/#/djradio?', 'https://music.163.com/djradio?', 'https://y.music.163.com/m/djradio?']
+    if (typeof url == 'string') {
+        if (url.includes('music')) {
             for (let i = 0; i < url_list.length; i++) {
                 if (url.indexOf(url_list[i]) == 0) {
                     if (i < 3) { return 17 }
                     if (i < 6) { return 2 }
-                    if (i < 9) { return 0 }
-                    if (i < 12) { return 1 }
+                    if (i < 10) { return 0 }
+                    if (i < 13) { return 1 }
                     return 4
                 }
             }
@@ -37,20 +37,25 @@ function searchURL(url, param) {
 }
 
 function addMusic() {
-    let url = prompt("歌曲网址")
+    let url = prompt('歌曲网址')
     let t = checkURL(url)
     if (t >= 0) {
-        let name = prompt("歌曲名")
-        let id = searchURL(url, "id")
+        let name = prompt('歌曲名')
+        let id = searchURL(url, 'id')
         let height = (t == 2) ? 66 : 430;
         let outchain = 'https://music.163.com/outchain/player?type=' + t + '&id=' + id + '&auto=1&height=' + height
-        alert('[url=' + url + ']' + name + '[/url][flash]' + outchain + '[/flash]')
+        switch (t) {
+            case 2: url = 'https://music.163.com/#/song?id=' + id; break;
+            case 0: url = 'https://music.163.com/#/playlist?id=' + id; break;
+            case 1: url = 'https://music.163.com/#/album?id=' + id; break;
+            case 4: url = 'https://music.163.com/#/djradio?id=' + id; break;
+        }
+        if (name == '' || name == null) { alert('[url]' + url + '[/url][flash]' + outchain + '[/flash]') }
+        else { alert('[url=' + url + ']' + name + '[/url][flash]' + outchain + '[/flash]') }
     }
 }
 
 (function () {
-    document.getElementById("moreconf").innerHTML += '<a href="javascript:;" id="addmusic">添加音乐</a>'
-    document.getElementById("addmusic").addEventListener("click", addMusic, false)
+    document.getElementById('moreconf').innerHTML += '<a href="javascript:;" id="addmusic">添加音乐</a>'
+    document.getElementById('addmusic').addEventListener('click', addMusic, false)
 })();
-
-
